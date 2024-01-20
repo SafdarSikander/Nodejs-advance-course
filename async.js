e9f0e0ff77012f2a81267c942083670998df93d4
Safdar Sikander <safdar_sikander@live.com>
2024-01-20 16:03:22 +0500

    Add Async example

---
+++ async.js
@@ -0,0 +1,25 @@
+const https = require('https');
+
+const start = Date.now();
+
+function doRequest() {
+  https.request('https://www.google.com.pk', res => {
+    res.on('data', () => {
+    })
+    res.on('end', () => {
+      console.log('Time:', Date.now() - start);
+    })
+  }).end();
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();
